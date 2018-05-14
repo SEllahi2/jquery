f26acf87e832c10d8a9a29c3042a26a8ea0cc7e2
SEllahi2 <SEllahi2@uclan.ac.uk>
2018-05-14 22:57:12 +0100

    Add files via upload

---
+++ jquery.js
@@ -0,0 +1,12 @@
+    $(document).ready(function(){
+      $('.slider').bxSlider();
+    }); //slider function used to move across and see the different images within the code...SE
+
+
+/* button to show and hide contact details*/
+
+    $(document).ready(function(){
+    $("button").click(function(){
+        $("p").toggle();
+    });
+}); 
